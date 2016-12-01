0a5ae65980caf49313bcb14df51fa250675ab507
tdillon <tdillon@users.noreply.github.com>
2016-12-01 12:30:27 -0500

    #22 Ranges class is updated with the exception of pressure.

--- app/src/main/java/tgd/mindless/drone/weatherwidgetnumberone/redux/Ranges.java
+++ app/src/main/java/tgd/mindless/drone/weatherwidgetnumberone/redux/Ranges.java
@@ -1,5 +1,8 @@
 package tgd.mindless.drone.weatherwidgetnumberone.redux;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Ranges {
 
     class Range {
@@ -12,19 +15,45 @@ class Ranges {
         }
     }
 
-    Range temperature = new Range(Integer.MAX_VALUE, Integer.MIN_VALUE);
+    Range temperature;
+    Range ozone;
+    Range pressure;
+    Range windSpeed;
+    Range precipAccumulation;
 
     Ranges(Weather.DataBlock db, ThemesClass theme) {
 
-        for (Weather.DataPoint dp : db.data) {
-            if (dp.temperatureMax > temperature.max) {
-                temperature.max = dp.temperatureMax;
+        Map<String, Range> pertinentOptions = new HashMap<>();
+        pertinentOptions.put("ozone", ozone);
+        pertinentOptions.put("windSpeed", windSpeed);
+        pertinentOptions.put("pressure", pressure);
+        pertinentOptions.put("precipAccumulation", precipAccumulation);
+        pertinentOptions.put("dewPoint", temperature);
+        pertinentOptions.put("temperature", temperature);
+        pertinentOptions.put("apparentTemperature", temperature);
+        pertinentOptions.put("temperatureMax", temperature);
+        pertinentOptions.put("temperatureMin", temperature);
+        pertinentOptions.put("apparentTemperatureMax", temperature);
+        pertinentOptions.put("apparentTemperatureMin", temperature);
+
+
+        for (ThemesClass.Property property : theme.properties) {
+            if (!pertinentOptions.containsKey(property.name)){
+                continue;  //e.g., moonPhase
             }
-            if (dp.temperatureMax < temperature.min) {
-                temperature.min = dp.temperatureMax;
+            Range r = pertinentOptions.get(property.name);
+            if (r == null) {
+                r = new Range(Integer.MAX_VALUE, Integer.MIN_VALUE);
+            }
+            for (Weather.DataPoint dataPoint : db.data) {
+                float value = dataPoint.getValue(property.name);
+                if (value > r.max) {
+                    r.max = value;
+                }
+                if (value < r.min) {
+                    r.min = value;
+                }
             }
         }
-
-
     }
 }
