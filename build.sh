d25703592563fd1dd23da1fa479e0519a8316c1f
tdillon <tdillon@users.noreply.github.com>
2016-12-01 06:48:23 -0500

    fixing build.sh to correct deployments to play store

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 set -ev
 
+echo "BRANCH: ("$TRAVIS_BRANCH")"
 echo "TAG: ("$TRAVIS_TAG")"
 
 ./gradlew assembleRelease
 
-if [ "${TRAVIS_BRANCH}" = "master" && "${TRAVIS_TAG}" != "" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_TAG}" != "" ]; then
   jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore tgd.android.jks app/build/outputs/apk/app-release-unsigned.apk weather-widget-number-one
   /usr/local/android-sdk/build-tools/23.0.3/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release.apk
   ls -la app/build/outputs/apk
