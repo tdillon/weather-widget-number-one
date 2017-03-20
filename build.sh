876593b743827c9448f88e9a55a870cc9a2ae780
tdillon <tdillon@users.noreply.github.com>
2017-03-20 19:38:53 -0400

    updating build.sh to build tools 25.0.0

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "TAG: ("$TRAVIS_TAG")"
 
 if [ "${TRAVIS_TAG}" != "" ]; then
   jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore tgd.android.jks app/build/outputs/apk/app-release-unsigned.apk weather-widget-number-one
-  /usr/local/android-sdk/build-tools/23.0.3/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release.apk
+  /usr/local/android-sdk/build-tools/25.0.0/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release.apk
   ls -la app/build/outputs/apk
   supply -v
   supply run -j "Google Play Android Developer-5d5bd2dcc1bd.json" -p tgd.mindless.drone.weatherwidgetnumberone.redux -b app/build/outputs/apk/app-release.apk
